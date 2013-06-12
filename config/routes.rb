a30d94dcb5b09f6c60b92a8adfecb42ae911f149
MichelleMHey <michellemhey@gmail.com>
2013-06-12 13:38:56 -0500

    edit controller files and modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ RubyClosetIi::Application.routes.draw do
 
   root :to => "home#index"
   devise_for :users
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
