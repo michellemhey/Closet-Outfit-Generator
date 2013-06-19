d20e3d30d29d6fed8c17e44f644c8f202b93c525
MichelleMHey <michellemhey@gmail.com>
2013-06-19 14:04:08 -0500

    passing rspec tests for clothing_spec.rb

--- spec/models/clothing_spec.rb
+++ spec/models/clothing_spec.rb
@@ -4,7 +4,13 @@ describe Clothing do
   it "has a valid factory" do
     FactoryGirl.create(:clothing).should be_valid
   end
-  it "is invalid without a name"
-  it "is invalid without a type"
-  it "is invalid without a user"
+  it "is invalid without a name" do
+    FactoryGirl.build(:clothing, name: nil).should_not be_valid
+  end
+  it "is invalid without a type" do
+    FactoryGirl.build(:clothing, name: nil).should_not be_valid
+  end
+  it "is invalid without a user" do
+    FactoryGirl.build(:clothing, name: nil).should_not be_valid
+  end
 end
