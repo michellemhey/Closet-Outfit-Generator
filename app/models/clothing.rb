00cd4dc4f9228bbc9d9a8d724ee121bda201f110
MichelleMHey <michellemhey@gmail.com>
2013-06-20 13:34:33 -0500

    changed 'has_one' to 'belongs_to'

--- app/models/clothing.rb
+++ app/models/clothing.rb
@@ -1,7 +1,6 @@
 class Clothing < ActiveRecord::Base
   attr_accessible :description, :image_path, :is_dirty, :name, :type_id, :user_id
-  has_one :type
+  belongs_to :type
   belongs_to :user
 
-  validates_presence_of :name
 end
