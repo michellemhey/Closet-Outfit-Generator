43caccb5d0aa7bbc8bd35e6b1307a1da9c030233
MichelleMHey <michellemhey@gmail.com>
2013-06-21 09:28:56 -0500

    added carrierwave

--- app/models/clothing.rb
+++ app/models/clothing.rb
@@ -1,6 +1,7 @@
 class Clothing < ActiveRecord::Base
-  attr_accessible :description, :image_path, :is_dirty, :name, :type_id, :user_id
+  attr_accessible :description, :image_path, :is_dirty, :name, :type_id, :user_id, :image
   belongs_to :type
   belongs_to :user
+  mout_uploader :image, ImageUploader
 
 end
