7fa4314c53a7a98607c052e4f10c8a1d6c28a8b4
MichelleMHey <michellemhey@gmail.com>
2013-06-22 16:16:43 -0500

    screwed up on carrierwave gem

--- app/models/photo.rb
+++
@@ -1,5 +0,0 @@
-class Photo < ActiveRecord::Base
-  attr_accessible :image, :property_id
-  belongs_to :property
-  mount_uploader :image, PhotoUploader
-end
--- app/models/property.rb
+++
@@ -1,3 +0,0 @@
-class Property < ActiveRecord::Base
-  has_many :photos, :dependent => destroy
-end
