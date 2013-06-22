27272555902da355bcd1065cd2effc42d4eaec7d
MichelleMHey <michellemhey@gmail.com>
2013-06-22 16:12:10 -0500

    screwed up on carrierwave gem

--- spec/factories/images.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :image do
-    image_id 1
-    image "MyString"
-  end
-end
--- spec/factories/photos.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :photo do
-    property_id 1
-    image "MyString"
-  end
-end
