154aaa1cb85e4850a8573c21516ffd4efb1858bd
MichelleMHey <michellemhey@gmail.com>
2013-07-02 10:47:57 -0500

    removed

--- features/step_definitions/fabrication_ext_steps.rb
+++
@@ -1,9 +0,0 @@
-Given /^I have (.*)$/ do |body, table|
-  assert @user
-  step "that user has #{body}", table
-end
-
-Given(/^"(.*?)" has (.*)$/) do |email, body, table|
-  @user = User.find_by_email(email) || Fabricate(:user, email: email)
-  step "that user has #{body}", table
-end
